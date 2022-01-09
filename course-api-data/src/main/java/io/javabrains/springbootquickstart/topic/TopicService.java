c1a6d3b061656b4b2b7e497f0070823a4d510ab1
shubhamjindal1 <37876107+shubhamjindal1@users.noreply.github.com>
2022-01-10 00:30:24 +0530

    Added files via upload

---
+++ course-api-data/src/main/java/io/javabrains/springbootquickstart/topic/TopicService.java
@@ -0,0 +1,45 @@
+package io.javabrains.springbootquickstart.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("java", "Core Java", "Core Java Description"),
+				new Topic("javascript", "JavaScript", "JavaScript Description")
+				));
+	
+	public List<Topic> getAllTopics(){
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics :: add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topicRepository.findById(id).orElse(null);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		topicRepository.save(topic);
+		
+	}
+
+	public void deleteTopic(String id) {
+		topicRepository.deleteById(id);
+		
+	}
+}
